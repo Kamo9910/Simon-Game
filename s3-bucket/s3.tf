0922165bd8de4faf203d99afd5dea1a156f4a146
Danny Kamohelo Ntshala <ntshaladannykamohelo@gmail.com>
2026-03-10 01:15:22 +0200

    Add files via upload

---
+++ s3-bucket/s3.tf
@@ -0,0 +1,54 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+  
+}
+
+resource "aws_s3_bucket" "static_website" {
+  bucket = "my-static-website-${random_id.bucket_suffix.hex}"
+  
+}
+
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_website_public_read" {
+    bucket = aws_s3_bucket.static_website.id
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+            Sid = "PublicReadGetObject"
+            Effect = "Allow"
+            Principal = "*"
+            Action = "s3:GetObject"
+            Resource = "${aws_s3_bucket.static_website.arn}/*"
+        }
+        ]
+    })
+
+    depends_on = [aws_s3_bucket.static_website.static_website]
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.static_website.id
+  key    = "index.html"
+  source = "build/index.html"
+  etag = filemd5("build/index.html")
+  content_type = "text/html"
+}
+
